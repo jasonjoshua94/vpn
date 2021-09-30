bbe12607832d2a3df6182d664e5d40ead2f92b5e
jasonjoshua94 <90880369+jasonjoshua94@users.noreply.github.com>
2021-09-30 11:14:18 +0800

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "#         Premium Auto Script By JAYVPN         #"
+echo -e "#-----------------------------------------------#"
+echo '                 _______  ___    ______  _   __ ' 
+echo '                / /   \ \/ / |  / / __ \/ | / / '
+echo '           __  / / /| |\  /| | / / /_/ /  |/ /  '
+echo '          / /_/ / ___ |/ / | |/ / ____/ /|  /   '
+echo '          \____/_/  |_/_/  |___/_/   /_/ |_/    '
+echo '         	         UNLIMITED INTERNET '
+echo '         	        CREDIT : CTECH DIDIK '
+echo '         	           CREDIT : SAMVPN '
+echo ''
+echo -e "================================================="
---
+++ info.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+clear
+neofetch --ascii_distro Minix
+echo ' Premium Autoscript By Jason Joshua '
+echo '        Edited by : SAMVPN  '
+echo ''
+cat /root/log-install.txt
+echo ""
